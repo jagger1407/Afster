f4f8912b2acd51d3a232a6cbf444b1e4c8cf270f
jagger1407 <jackbroek10@gmail.com>
2024-12-11 19:37:48 +0100

    Add files

---
+++ types.h
@@ -0,0 +1,26 @@
+#ifndef TYPES_H_INCLUDED
+#define TYPES_H_INCLUDED
+
+typedef unsigned char u8;
+typedef char s8;
+
+typedef unsigned short u16;
+typedef short s16;
+
+typedef unsigned int u32;
+typedef int s32;
+
+typedef unsigned long long u64;
+typedef long long s64;
+
+typedef unsigned char uchar;
+typedef unsigned short ushort;
+typedef unsigned long ulong;
+typedef unsigned long long ulonglong;
+typedef unsigned int uint;
+
+#include <stdbool.h>
+
+#define nullptr ((void*)0x00)
+
+#endif // TYPES_H_INCLUDED
