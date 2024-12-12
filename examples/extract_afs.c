c6b801b5e3166beb6a0120bba1b287e7a26bdbed
jagger1407 <jackbroek10@gmail.com>
2024-12-12 15:17:46 +0100

    Added more examples, slight tweak to importAfl
    created examples folder
    moved example.c -> examples/extract_afs.c
    created display_afs.c to showcase displaying AFS info
    created import_afl.c to showcase importing AFL lists
    added a file count mismatch check between AFS and AFL to importAfl
    changed README to reflect the new example folder

---
+++ examples/extract_afs.c
@@ -0,0 +1,70 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "../afs.h"
+
+/*
+ * This is an example program used to demonstrate how one can use this library.
+ * In this case, we extract the entire AFS File into a folder.
+ *
+ * This program takes in 3 arguments:
+ * arg1 = A path to an AFS file
+ * arg2 = Either a path to an AFL file or the letter 'n' in case no AFL is wanted
+ * arg3 = A path to an output folder where all entries are to be stored
+*/
+
+int main(int argc, char** argv) {
+    // Checking if all arguments are present
+    if(argc < 2) {
+        puts("ERROR: main - No AFS file specified.");
+        return 1;
+    }
+    else if(argc < 3) {
+        puts("ERROR: main - No AFL option specified.");
+        return 2;
+    }
+    else if(argc < 4) {
+        puts("ERROR: main - No output path specified.");
+        return 3;
+    }
+
+    // Now that we know all arguments are there,
+    // we can create an AFS Handle with the first argument
+    // using the openAfs() function.
+    Afs_t* afs = openAfs(argv[1]);
+    if(afs == NULL) {
+        puts("ERROR: main - AFS file couldn't be created.");
+        return 1;
+    }
+
+    if(*argv[2] != 'n') {
+        // In case it is wanted, we create an AFL Handle using openAfl()
+        Afl_t* afl = openAfl(argv[2]);
+        if(afl == NULL) {
+            puts("ERROR: main - AFL file couldn't be created.");
+            return 2;
+        }
+        // Then we import the AFL list into the metadata section of the AFS,
+        // but since we pass false here, the actual AFS File isn't updated,
+        // it is only imported in-memory.
+        importAfl(afs, afl, false);
+
+        // Lastly, to ensure there is no memory leak,
+        // we free the AFL since we aren't using it anymore.
+        freeAfl(afl);
+    }
+
+    // Now we check the validity of the output folder.
+    if(argv[3] == NULL || *argv[3] == 0x00) {
+        puts("ERROR: main - Output folder doesn't exist or is invalid");
+        printf("Output folder: %s\n", argv[3]);
+        freeAfs(afs);
+        return 3;
+    }
+
+    // And this one function call here fully extracts the entire AFS File.
+    extractWholeAfs(afs, argv[3]);
+
+    // Lastly we make sure that no memory leaks by freeing all AFS related memory.
+    freeAfs(afs);
+    return 0;
+}
