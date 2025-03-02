a05e085fc31f00c1817c428e65f4ffe5d6674247
jagger1407 <jackbroek10@gmail.com>
2025-03-02 22:50:15 +0100

    Update Afster examples
    - Added check for a valid AFS filepath in all examples
    - Added help text and printHelp() function in all examples

--- examples/rename_entry.c
+++ examples/rename_entry.c
@@ -2,6 +2,15 @@
 #include <stdlib.h>
 #include "../afs.h"
 
+/** Prints a help text explaining how to use this program
+ */
+void printHelp() {
+    puts("rename - Rename an entry of the AFS.\n");
+    puts("arg1 = A path to an AFS File");
+    puts("arg2 = The index of the entry whose name we're changing");
+    puts("arg3 = The new name.");
+}
+
 /*
  * This is an example program used to demonstrate how one can use this library.
  * In this case, we rename one singular entry within the AFS.
@@ -11,20 +20,30 @@
  * arg2 = The index of the entry whose name we're changing
  * arg3 = The new name.
 */
-
 int main(int argc, char** argv) {
     // Checking if all arguments are present
     if(argc < 2) {
         puts("ERROR: main - No AFS file specified.");
+        printHelp();
         return 1;
     }
     else if(argc < 3) {
         puts("ERROR: main - No entry index specified.");
+        printHelp();
         return 2;
     }
     else if(argc < 4) {
         puts("ERROR: main - No name specified.");
+        printHelp();
         return 3;
+    }
+
+    // Checking whether the given path points to an AFS File
+    int len = strlen(argv[1]);
+    if(strcmp(strlwr(argv[1]) + len - 4, ".afs") != 0) {
+        puts("ERROR: main - arg1 is not an AFS File.");
+        printHelp();
+        return 1;
     }
 
     // Now that we know all arguments are there,
